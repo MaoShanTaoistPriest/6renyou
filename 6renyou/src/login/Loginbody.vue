64478cc7a80f4969d00d92bc1cfa70efe17be759
zwj <zhangwenjuan0202@163.com>
2019-09-12 17:15:30 +0800

    d

--- 6renyou/src/login/Loginbody.vue
+++ 6renyou/src/login/Loginbody.vue
@@ -5,8 +5,22 @@
             <div class="login-block">
                 <!-- 此处套用el标签页组件 -->
                 <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
-                    <el-tab-pane label="登录" name="first">登录页面</el-tab-pane>
-                    <el-tab-pane label="注册" name="second">注册页面</el-tab-pane>
+                    <el-tab-pane label="登录" name="first">
+                        <ul>
+                            <li class="li_uname">
+                                <div>用户名</div>
+                                <el-input></el-input>
+                                <span><img src="" alt="">用户名不能为空</span>
+                                <!-- <span><img src="" alt="">正确</span> -->
+                            </li>
+                            <li>
+                                <el-button>登录</el-button>
+                            </li>
+                        </ul>
+                    </el-tab-pane>
+                    <el-tab-pane label="注册" name="second">
+                        注册页面
+                    </el-tab-pane>
                 </el-tabs>
             </div>
         </div>
@@ -39,33 +53,40 @@ export default {
   position: relative;
 }
 .loginbody .box .login-block {
-  width: 426px;
-  height: 410px;
+  width: 460px;
+  height: 460px;
   background-color: #fff;
   border-radius: 8px;
   position: absolute;
-  top: 70px;
-  left: 50px;
+  top: 30px;
+  left: 20px;
 }
 /*修改el标签页组价默认样式*/
 .el-tabs .el-tabs__header .el-tabs__item{
-    width:212px;height:60px;
+    width:229.2px;height:60px;
     line-height:60px;
     background-color:#ebebeb;
     color:#333;
     font-size:20px;
     text-align: center;
     border:0;
-    border-top-right-radius:6px;
+    border-top-right-radius:4px;
 }
 .el-tabs .el-tabs__header .el-tabs__item:first-child{
     border-radius:0;
     border-top-left-radius:6px;
 }
+/*标签页选中时的样式*/
 .el-tabs .el-tabs__header .el-tabs__item.is-active{
     border:0;
     color:#fff;
     background-color:#3eb166;
+}
+/*表单*/
+ul,li{list-style:none;}
+ul .li_uname div,ul .li_uname .el-input,ul .li_uname span{float:left;color:#333;font-size:14px;}
+ul .li_uname .el-input{
+    width:200px;
 }
 </style>
 
