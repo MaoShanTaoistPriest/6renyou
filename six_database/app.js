9ea50b2265496f205717f298e4509380d69b4b8b
joker <871099553@qq.com>
2019-09-14 19:05:23 +0800

    usercomment update

--- six_database/app.js
+++ six_database/app.js
@@ -235,10 +235,10 @@ server.get("/indexConsultant", (req, res) => {
   })
 });
 
-// 客户评价的数据的获取,通过id降序的方式，获取的数据从最后录入的数据开始
+// 客户评价的数据的获取,通过id降序的方式，返回值为数组长度length，用于分页查询组件的总体长度，返回数据为id倒序
 server.get("/userComment", (req, res) => {
-  var sql = "SELECT id,img,title,pName,pubDate,feeling FROM six_user_comment ORDER BY id DESC";
-  pool.query(sql, (err, result) => {
+  var sql1 = `SELECT id FROM six_user_comment`;
+  pool.query(sql1, (err, result) => {
     if (err) throw err;
     if (result.length == 0) {
       res.send({
@@ -246,11 +246,33 @@ server.get("/userComment", (req, res) => {
         msg: "查询有误"
       });
     } else {
-      res.send({
-        code: "1",
-        msg: "查询成功",
-        data: result
-      });
+      var pno = req.query.pno;
+      var pcount = req.query.pcount;
+      if (!pno) {
+        pno = 1;
+      };
+      if (!pcount) {
+        pcount = 6;
+      };
+      var pnum = result.length;
+      var start = pnum - (pno - 1) * pcount;
+      var sql2 = `SELECT id,img,title,pName,pubDate,feeling FROM six_user_comment WHERE id <= ${start} ORDER BY id DESC limit ${pcount}`;
+      pool.query(sql2, (err, result) => {
+        if (err) throw err;
+        if (result.length == 0) {
+          res.send({
+            code: "-1",
+            msg: "查询有误"
+          });
+        } else {
+          res.send({
+            code: "1",
+            msg: "查询成功",
+            length: pnum,
+            data: result
+          });
+        }
+      })
     }
   })
 });
