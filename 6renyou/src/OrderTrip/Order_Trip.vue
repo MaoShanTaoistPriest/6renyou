be38d97a2ba8c3e196df2b13ba96b30201b069f0
huangpeng <1009425356@qq.com>
2019-09-17 18:35:10 +0800

    test

---
+++ 6renyou/src/OrderTrip/Order_Trip.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="OrderTrip-container-box">
+        <div class="section">
+            <div class="fp-tableCell">
+                <div class="section-inner"></div>
+            </div>
+        </div>
+    </div>
+</template>
--- 6renyou/src/views/OrderTrip.vue
+++ 6renyou/src/views/OrderTrip.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <myheader></myheader>
+    <Order-Trip></Order-Trip>
     <my-footer></my-footer>
   </div>
 </template>
 <script>
+import OrderTrip from "../OrderTrip/Order_Trip.vue"
 export default {
-  
+  components:{
+    "Order-Trip":OrderTrip
+  }
 }
 </script>
 <style scoped>
